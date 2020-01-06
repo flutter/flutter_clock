afee6e7f1b3c4e56b7104387db750739e478d2a5
Florent Pergoud <florentp@bam.tech>
2020-01-06 19:37:21 +0100

    :sparkles: (minimalist clock) initial commit

---
+++ minimalist_clock/lib/main.dart
@@ -0,0 +1,34 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter_clock_helper/customizer.dart';
+import 'package:flutter_clock_helper/model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'digital_clock.dart';
+
+void main() {
+  // A temporary measure until Platform supports web and TargetPlatform supports
+  // macOS.
+  if (!kIsWeb && Platform.isMacOS) {
+    // TODO(gspencergoog): Update this when TargetPlatform includes macOS.
+    // https://github.com/flutter/flutter/issues/31366
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override.
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+
+  // This creates a clock that enables you to customize it.
+  //
+  // The [ClockCustomizer] takes in a [ClockBuilder] that consists of:
+  //  - A clock widget (in this case, [DigitalClock])
+  //  - A model (provided to you by [ClockModel])
+  // For more information, see the flutter_clock_helper package.
+  //
+  // Your job is to edit [DigitalClock], or replace it with your
+  // own clock widget. (Look in digital_clock.dart for more details!)
+  runApp(ClockCustomizer((ClockModel model) => DigitalClock(model)));
+}
