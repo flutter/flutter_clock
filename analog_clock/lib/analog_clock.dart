f73016547cf7e0844202ebd295f570ae836054c4
Eria <c121680118@gmail.com>
2020-01-10 03:07:15 +0800

    加圖

--- analog_clock/lib/analog_clock.dart
+++ analog_clock/lib/analog_clock.dart
@@ -136,44 +136,47 @@ class _AnalogClockState extends State<AnalogClock> {
         color: customTheme.backgroundColor,
         child: Stack(
           children: [
+            Image(
+              image: AssetImage("../images/record_2.png"),
+            ),
             // Example of a hand drawn with [CustomPainter].
-            DrawnHand(
-              color: customTheme.accentColor,
-              thickness: 4,
-              size: 1,
-              angleRadians: _now.second * radiansPerTick,
-            ),
-            DrawnHand(
-              color: customTheme.highlightColor,
-              thickness: 16,
-              size: 0.9,
-              angleRadians: _now.minute * radiansPerTick,
-            ),
-            // Example of a hand drawn with [Container].
-            ContainerHand(
-              color: Colors.transparent,
-              size: 0.5,
-              angleRadians: _now.hour * radiansPerHour +
-                  (_now.minute / 60) * radiansPerHour,
-              child: Transform.translate(
-                offset: Offset(0.0, -60.0),
-                child: Container(
-                  width: 32,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    color: customTheme.primaryColor,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              left: 0,
-              bottom: 0,
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: weatherInfo,
-              ),
-            ),
+            // DrawnHand(
+            //   color: customTheme.accentColor,
+            //   thickness: 4,
+            //   size: 1,
+            //   angleRadians: _now.second * radiansPerTick,
+            // ),
+            // DrawnHand(
+            //   color: customTheme.highlightColor,
+            //   thickness: 16,
+            //   size: 0.9,
+            //   angleRadians: _now.minute * radiansPerTick,
+            // ),
+            // // Example of a hand drawn with [Container].
+            // ContainerHand(
+            //   color: Colors.transparent,
+            //   size: 0.5,
+            //   angleRadians: _now.hour * radiansPerHour +
+            //       (_now.minute / 60) * radiansPerHour,
+            //   child: Transform.translate(
+            //     offset: Offset(0.0, -60.0),
+            //     child: Container(
+            //       width: 32,
+            //       height: 150,
+            //       decoration: BoxDecoration(
+            //         color: customTheme.primaryColor,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            // Positioned(
+            //   left: 0,
+            //   bottom: 0,
+            //   child: Padding(
+            //     padding: const EdgeInsets.all(8),
+            //     child: weatherInfo,
+            //   ),
+            // ),
           ],
         ),
       ),
